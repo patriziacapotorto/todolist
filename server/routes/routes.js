0aa97b16309ea63ddd530c4796bd23cf5da1a115
CAPOTORTO\Patrizia <patriziacapotorto1@gmail.com>
2017-03-16 17:36:40 +0100

    aggiunte rotte per il client in routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,15 @@
 module.exports= function(app, express, path){
-// app.use('/js',express.static(path.join(__dirname,"..","..","client","js")));
-// app.use('/css',express.static(path.join(__dirname,"..","..","client","css")));
-// app.get('/', function(req,res){
-//   res.sendFile(path.join(__dirname,"..","..","client","index.html"));
-// });
+app.use('/js',express.static(path.join(__dirname,"..","..","client","js")));
+app.use('/css',express.static(path.join(__dirname,"..","..","client","css")));
+app.use('/bootsrap',express.static(path.join(__dirname,"..","..","node_modules","bootsrap","dist")));
+app.use('/angular',express.static(path.join(__dirname,"..","..","node_modules","angular")));
+app.use('/angular-ui-router',express.static(path.join(__dirname,"..","..","node_modules","angular-ui-router")));
+app.use('/jquery',express.static(path.join(__dirname,"..","..","node_modules","jquery","dist")));
+
+//ROTTA PER L'INDEX
+app.get('/', function(req,res){
+  res.sendFile(path.join(__dirname,"..","..","client","index.html"));
+});
 var bodyparser = require('body-parser');
 app.use(bodyparser.json());
 
